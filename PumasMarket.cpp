bf3a403d1a65a4e977c69d54430a6e9021d02add
River6225 <krisriver62@gmail.com>
2025-06-18 00:15:29 -0600

    Se añadio el menu

---
+++ PumasMarket.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void menuBienvenida (){
+    
+    cout << "     Bienvenido a PUMAS SUPER MARKET \n ";
+}
+
+int main(){
+       
+ menuBienvenida ();
+
+    return 0;
+}
